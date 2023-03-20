bd9e8af572949d64aa1dfa5ab45ce03b0bae4838
zaman <zamanankon2@gmail.com>
2023-03-20 18:45:11 +0600

    new

---
+++ app.js
@@ -0,0 +1,34 @@
+const loadMeals=(searchText)=>{
+    const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    console.log(url);
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayMeals(data.meals))
+}
+
+ const displayMeals=(meals)=>{
+    const mealsContainer=document.getElementById('meals-container')
+    mealsContainer.innerHTML='';
+     meals.forEach(meal=>{
+     const mealDiv=document.createElement('div')
+        mealDiv.classList.add('col')
+        mealDiv.innerHTML=`
+         <div class="col">
+        <div class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.</p>
+            </div>
+        </div>
+    </div>
+        `
+      mealsContainer.appendChild(mealDiv)
+     })   
+ }
+const  searchMeals=()=>{
+  const searchText=document.getElementById('search-field').value
+  loadMeals(searchText);
+}
+loadMeals('fish')
