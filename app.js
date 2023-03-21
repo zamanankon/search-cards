dfeab3243d64131bd3e67e70f99db475582a5fc3
zaman <zamanankon2@gmail.com>
2023-03-21 15:46:50 +0600

    add

--- app.js
+++ app.js
@@ -14,12 +14,15 @@ const loadMeals=(searchText)=>{
         mealDiv.classList.add('col')
         mealDiv.innerHTML=`
          <div class="col">
-        <div class="card h-100">
+        <div class="card h-80">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional
                     content. This content is a little bit longer.</p>
+                  <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+Details
+</button>
             </div>
         </div>
     </div>
@@ -30,5 +33,23 @@ const loadMeals=(searchText)=>{
 const  searchMeals=()=>{
   const searchText=document.getElementById('search-field').value
   loadMeals(searchText);
+}
+
+const loadMealDetail=idMeal=>{
+    const url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+     fetch(url)
+     .then(res=>res.json())
+     .then(data=>displayDetails(data.meals[0]))
+}
+
+const displayDetails=meal=>{
+   const data=document.getElementById('exampleModalLabel').innerText=meal.strMeal;
+   console.log(data);
+    const mealsDetails=document.getElementById('mealDetailsBody')
+    console.log(mealsDetails);
+    mealsDetails.innerHTML=`
+    <img class="img-fluid" src="${meal.strMealThumb}">
+ 
+    `
 }
 loadMeals('fish')
